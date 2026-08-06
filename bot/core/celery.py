316b3bd9ad1eafbec151364b18748f1c48fa0f9f
RiaLnN <max07070799@gmail.com>
2026-08-06 02:06:11 +0200

    update celery initialization

--- bot/core/celery.py
+++ bot/core/celery.py
@@ -1,4 +1,17 @@
 from celery import Celery
 from bot.core.config import settings
 
-app = Celery('llm_call', broker=settings.redis_dsn)
+app = Celery(
+    "whisper_bot",
+    broker=settings.redis_dsn,
+    backend=settings.redis_dsn,
+    include=["bot.tasks"]
+)
+
+
+if settings.redis_dsn.startswith("rediss://"):
+    import ssl
+    app.conf.update(
+        broker_use_ssl={'ssl_cert_reqs': ssl.CERT_NONE},
+        redis_backend_use_ssl={'ssl_cert_reqs': ssl.CERT_NONE}
+    )
